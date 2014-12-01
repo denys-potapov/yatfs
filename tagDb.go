6aef69ca5e2f92cb140d2953915127b03ef6fa94
Денис <denys.potapov@gmail.com>
2014-12-01 23:49:49 +0200

    unfinished tagDB

--- tagDb.go
+++ tagDb.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"github.com/boltdb/bolt"
+	"bazil.org/fuse"
 )
 
 type TagDB struct {
 	db *bolt.DB
 }
 
-func NewTagDB(db *bolt.DB) *TagDB {
-	tagDB := TagDB{db}
+type inode fuse.NodeID
 
-	return &tagDB
+func NewTagDB(db *bolt.DB) *TagDB {
+
+	db.Update(func(tx *bolt.Tx) error {
+		buckets := []string{"tags", "inodes", "relations"}
+		for _, name := range buckets {
+			_, err := tx.CreateBucketIfNotExists([]byte(name))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	t := TagDB{db}
+
+	return &t
+}
+
+func getTagId() inode {
+
+}
+
+func (t *TagDB) AddFile(tags []string, path string) (inode, error) {
+
 }
