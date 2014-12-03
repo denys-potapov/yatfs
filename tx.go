0aac8d3f107123af20c56ceb5827ae5e15c884e5
Denys Potapov <denys.potapov@gmail.com>
2014-12-03 16:39:00 +0200

    add tx and testes

--- tx.go
+++ tx.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"bazil.org/fuse"
-	"encoding/binary"
+	"github.com/boltdb/bolt"
 )
 
 type Tx struct {
@@ -11,29 +10,46 @@ type Tx struct {
 
 func (tx *Tx) addInode(value string) Inode {
 	b := tx.Bucket([]byte("inodes"))
-	s, err := b.NextSequence()
+	i, err := b.NextSequence()
 	if err != nil {
 		return nil
 	}
-	inode := Inode(s)
-	b.Put(inode.Bytes(), []byte(value))
+	inode := NewInode(i)
+	b.Put(inode, []byte(value))
+
 	return inode
 }
 
-func (tx *tx) addTag(name string) (Inode, error) {
-	inode := addInode(name)
+func (tx *Tx) AddTag(name string) (Inode, error) {
+	inode := tx.addInode(name)
 	b := tx.Bucket([]byte("tags"))
-	err := b.Put([]byte(name), inode.Bytes())
+	err := b.Put([]byte(name), inode)
 
 	return inode, err
 }
 
-func (tx *Tx) findTag(name string) Inode {
+func (tx *Tx) FindTag(name string) Inode {
 	b := tx.Bucket([]byte("tags"))
-	bytes := b.Get([]byte(name))
-	if bytes == nil {
-		return nil
+	inode := b.Get([]byte(name))
+
+	return inode
+}
+
+func (tx *Tx) addRelation(i1 Inode, i2 Inode, weight byte) error {
+	b := tx.Bucket([]byte("relations"))
+	item, err := b.CreateBucketIfNotExists(i1)
+	if err != nil {
+		return err
 	}
 
-	return NewInode(bytes)
+	return item.Put(i2, []byte{weight})
+}
+
+func (tx *Tx) AddRelation(file Inode, tag Inode, weight byte) error {
+	err := tx.addRelation(file, tag, weight)
+	if err != nil {
+		return err
+	}
+	err = tx.addRelation(tag, file, weight)
+	return err
 }
---
+++ tx_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"github.com/boltdb/bolt"
+	"testing"
+)
+
+func withTx(t *testing.T, fn func(*Tx)) {
+	withDB(t, func(db *bolt.DB) {
+		d := InitDB(db)
+		d.update(func(tx *Tx) error {
+			fn(tx)
+			return nil
+		})
+	})
+}
+
+func TestAddTag(t *testing.T) {
+	withTx(t, func(tx *Tx) {
+		inode, _ := tx.AddTag("tag")
+
+		i := tx.Bucket([]byte("inodes"))
+		if g, e := string(i.Get(inode)), "tag"; g != e {
+			t.Error("error adding tag inode")
+		}
+
+		b := tx.Bucket([]byte("tags"))
+		if g, e := b.Get([]byte("tag")), inode; !bytes.Equal(g, e) {
+			t.Errorf("error adding tag %v %v", g, e)
+		}
+	})
+}
+
+func TestFindTag(t *testing.T) {
+	withTx(t, func(tx *Tx) {
+		inode := NewInode(1)
+		_ = tx.Bucket([]byte("tags")).Put([]byte("tag"), inode)
+
+		if g, e := tx.FindTag("tag"), inode; !bytes.Equal(g, e) {
+			t.Error("couldnt find tag")
+		}
+	})
+}
+
+func TestAddRelation(t *testing.T) {
+	withTx(t, func(tx *Tx) {
+		file, tag, weight := NewInode(1), NewInode(2), byte(5)
+
+		_ = tx.AddRelation(file, tag, weight)
+
+		b := tx.Bucket([]byte("relations"))
+
+		tags := b.Bucket(file)
+		if g, e := tags.Get(tag), []byte{weight}; !bytes.Equal(g, e) {
+			t.Errorf("error adding file tags")
+		}
+
+		files := b.Bucket(tag)
+		if g, e := files.Get(file), []byte{weight}; !bytes.Equal(g, e) {
+			t.Errorf("error adding tags file")
+		}
+	})
+}
